c5f1a8951644151f76d2be909a7e666e0936dded
SkwiggumsMcD <80929936+SkwiggumsMcD@users.noreply.github.com>
2021-03-28 00:02:39 -0400

    Update project3_1.0.py

--- project3_1.0.py
+++ project3_1.0.py
@@ -84,36 +84,25 @@ class preprocess:
         genderlabels=np.unique(train_labels[:,2])
         racelabels=np.unique(train_labels[:,3])
         categories=[agelabels,genderlabels,racelabels]#all possible categories to be classified
-        return train_images,train_labels,val_images,val_labels,categories
+        return train_images,train_labels,val_images,val_labels
 
-class labelvalues:#for replacing string labels with numeric or numeric labels with string
-    def _init_(self):
+class cleandata:#for converting y-data for training or use in confusion matrix
+    def _init_(self,):
         print('init')
-    #encode array should be one of the category label arrays, data array should be one of the
-    #label arrays used in training or validation
-    def encode(self,dataarray,encodearray):
-        self.dataarray=dataarray
-        self.encodearray=encodearray
-        for i in range(np.size(self.encodearray)):
-            indices=(self.dataarray[:]==self.encodearray[i])
-            self.dataarray[indices]=i
-        return self.dataarray
-    def extract(self,dataarray,encodearray):
-        self.dataarray=dataarray
-        self.encodearray=encodearray
-        for i in range(np.size(self.encodearray)):
-            indices=(self.dataarray[:]==i)
-            self.dataarray[indices]=self.encodearray[i]
-        return self.dataarray
-
-
-
+    def training(self,inputarray):
+        inputarray,b=pd.factorize(inputarray)
+        inputarray=keras.utils.to_categorical(inputarray)
+        return inputarray.astype('int32')
+    def cmatrix(self,inputarray):
+        inputarray=inputarray
+        inputarray,matrixlabels=pd.factorize(inputarray)
+        inputarray=inputarray.astype('int64')
+        return inputarray,matrixlabels
 
 ## Task 1, FCNN
 class networks: 
-    def __init__(self,tasknum,xtrain,ytrain,xval,yval,categories):
+    def __init__(self,xtrain,ytrain,xval,yval):
         print('init')#never prints
-        self.tasknum=tasknum
         self.xtrain=xtrain
         self.xtrain=self.xtrain.astype('float32')
         self.ytrain=ytrain
@@ -121,31 +110,20 @@ class networks:
         self.xval=self.xval.astype('float32')
         self.yval=yval
         self.yvaltruth=yval
-        self.categories=categories
-
-        
-
-        #converting strings to numeric so Keras can use labels
-        self.ytrain=self.ytrain[:,self.tasknum+1]
-        self.yval=self.yval[:,self.tasknum+1]
-        self.yvaltruth=self.yvaltruth[:,self.tasknum+1]
-        encode=labelvalues()
-        self.ytrain,b=pd.factorize(self.ytrain)
-        self.ytrain=keras.utils.to_categorical(self.ytrain)
-        self.ytrain=self.ytrain.astype('int32')
-        self.yval,b=pd.factorize(self.yval)
-        self.yval=keras.utils.to_categorical(self.yval)
-        self.yval=self.yval.astype('int32')
-        self.yvaltruth=encode.encode(self.yvaltruth,categories[self.tasknum])
-        #self.yvaltruth,b=pd.factorize(self.yvaltruth)
-        #self.yvaltruth=keras.utils.to_categorical(self.yvaltruth)
-        self.yvaltruth=self.yvaltruth.astype('int64')
-
-    def fcnn(self,lr=0.1,lrfactor=0.2,mom=0.1,epochs=2):#lr=learning rate, lrd=learning rate decay, mom=momentum,tasknum=what to classify
+####################################################################################################       
+    def fcnn(self,tasknum=0,lr=0.5,lrfactor=0.2,mom=0.1,epochs=5):#lr=learning rate, lrd=learning rate decay, mom=momentum,tasknum=what to classify
+        self.tasknum=tasknum
         self.lr=lr
         self.lrfactor=lrfactor
         self.mom=mom
         self.epochs=epochs
+
+        #initializing values
+        self.clean=cleandata()
+        self.ytrain=self.clean.training(self.ytrain[:,self.tasknum+1])
+        self.yval=self.clean.training(self.yval[:,self.tasknum+1])
+        self.yvaltruth,self.cm_plot_labels=self.clean.cmatrix(self.yvaltruth[:,self.tasknum+1])
+        
 
         #network set-up 
         model=keras.Sequential()
@@ -170,7 +148,7 @@ class networks:
         #training and validating
         print('training and validating')
         reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=self.lrfactor, patience=1, min_lr=0.0001,)
-        history=model.fit(self.xtrain,self.ytrain,validation_split=0.1,epochs=self.epochs,callbacks=[reduce_lr])#if you don't specify a batch size, it uses 32 for mini-batch GD
+        history=model.fit(self.xtrain,self.ytrain,validation_split=0.2,epochs=self.epochs,callbacks=[reduce_lr])#if you don't specify a batch size, it uses 32 for mini-batch GD
         #testing
         print('evalauting')
         results=model.evaluate(self.xval,self.yval)
@@ -204,46 +182,289 @@ class networks:
         plt.xlabel('epoch')
         plt.legend(['train', 'validation'], loc='upper left')
         plt.show()
-
         trainingloss=history.history['loss']
         validationloss=history.history['val_loss']
-
         print('final validation accuracy is %f' %results[1])
-
-
+        
         confmatrix=sklearn.metrics.confusion_matrix(self.yvaltruth,predictions)
-        cm_plot_labels=self.categories[self.tasknum]
         self.tasktypes=['age confusion matrix','Gender confusion matrix','race confusion matrix']
         self.tasktypes=self.tasktypes[self.tasknum]
-        cmdisp=sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix=confmatrix,display_labels=cm_plot_labels)
+        cmdisp=sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix=confmatrix,display_labels=self.cm_plot_labels)
         cmdisp.plot()
         plt.show()
         return [trainingloss,validationloss,history,results,confmatrix]
 
+#############################################################################################
+    def cnn(self,tasknum=0,lr=0.5,lrfactor=0.2,mom=0.1,epochs=10):
+        self.tasknum=tasknum
+        self.lr=lr
+        self.lrfactor=lrfactor
+        self.mom=mom
+        self.epochs=epochs
+        self.xtrain=np.expand_dims(self.xtrain.reshape(86744,32,32), axis=3)#reshaping for convolution
+        self.xval=np.expand_dims(self.xval.reshape(10954,32,32), axis=3)#reshaping for convolution
+        
+        #initializing values
+        self.clean=cleandata()
+        self.ytrain=self.clean.training(self.ytrain[:,self.tasknum+1])
+        self.yval=self.clean.training(self.yval[:,self.tasknum+1])
+        self.yvaltruth,self.cm_plot_labels=self.clean.cmatrix(self.yvaltruth[:,self.tasknum+1])
+        
+        
+        model=keras.Sequential()
+        model.add(layers.Conv2D(filters=40,kernel_size=5,activation="relu",input_shape=(32,32,1)))
+        model.add(layers.MaxPooling2D())
+        model.add(layers.Flatten())
+        model.add(layers.Dense(100, activation="relu"))
+        if self.tasknum==0:#classify based on age
+            model.add(layers.Dense(9,activation="softmax"))
+        elif self.tasknum==1:#classify based on gender
+            model.add(layers.Dense(2,activation="softmax"))
+        else:#classify based on race
+            model.add(layers.Dense(7,activation="softmax"))
+        model.summary()
+    
+        model.compile(
+        optimizer=keras.optimizers.SGD(learning_rate=self.lr,momentum=self.mom),#default lr=0.01, default mom=0.0
+        loss=keras.losses.CategoricalCrossentropy(),
+        metrics=[keras.metrics.CategoricalAccuracy()],
+        )
+        
+        #training and validating
+        print('training and validating')
+        reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=self.lrfactor, patience=1, min_lr=0.0001,)
+        history=model.fit(self.xtrain,self.ytrain,validation_split=0.2,epochs=self.epochs,callbacks=[reduce_lr])#if you don't specify a batch size, it uses 32 for mini-batch GD
+        #testing
+        print('evalauting')
+        results=model.evaluate(self.xval,self.yval)
+        print('predicting')
+        #predicting
+        predictions = model.predict_classes(self.xval)
+        
+        #plotting/final results summary, from https://machinelearningmastery.com/display-deep-learning-model-training-history-in-keras/
+        plt.plot(history.history['categorical_accuracy'])
+        plt.plot(history.history['val_categorical_accuracy'])
+        if self.tasknum==0:#classify based on age
+            plt.title('model accuracy for age classification')
+        elif self.tasknum==1:#classify based on gender
+            plt.title('model accuracy for gender classification')
+        else:#classify based on race
+            plt.title('model accuracy for race classification')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
 
-    def cnn(self):
-        print('init')
-    def cnnhomebrew(self):
-        print('init')
-    def cnntwo(self):
-        print('init')
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        if self.tasknum==0:#classify based on age
+            plt.title('model loss for age classification')
+        elif self.tasknum==1:#classify based on gender
+            plt.title('model loss for gender classification')
+        else:#classify based on race
+            plt.title('model loss for race classification')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
+        trainingloss=history.history['loss']
+        validationloss=history.history['val_loss']
+        print('final validation accuracy is %f' %results[1])
+        
+        confmatrix=sklearn.metrics.confusion_matrix(self.yvaltruth,predictions)
+        self.tasktypes=['age confusion matrix','Gender confusion matrix','race confusion matrix']
+        self.tasktypes=self.tasktypes[self.tasknum]
+        cmdisp=sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix=confmatrix,display_labels=self.cm_plot_labels)
+        cmdisp.plot()
+        plt.show()
+        return [trainingloss,validationloss,history,results,confmatrix]
+################################################################################################
+    def cnnhomebrew(self,tasknum=0,lr=0.1,lrfactor=0.2,mom=0.2,epochs=10):
+        self.tasknum=tasknum
+        self.lr=lr
+        self.lrfactor=lrfactor
+        self.mom=mom
+        self.epochs=epochs
+        self.xtrain=np.expand_dims(self.xtrain.reshape(86744,32,32), axis=3)#reshaping for convolution
+        self.xval=np.expand_dims(self.xval.reshape(10954,32,32), axis=3)#reshaping for convolution
+        
+        #setting up inputs
+        self.clean=cleandata()
+        self.ytrain=self.clean.training(self.ytrain[:,self.tasknum+1])
+        self.yval=self.clean.training(self.yval[:,self.tasknum+1])
+        self.yvaltruth,self.cm_plot_labels=self.clean.cmatrix(self.yvaltruth[:,self.tasknum+1])
+
+        model=keras.Sequential()
+        model.add(layers.Conv2D(filters=20,kernel_size=3,activation="relu",input_shape=(32,32,1)))
+        #model.add(layers.MaxPooling2D())
+        model.add(layers.Conv2D(filters=40,kernel_size=5,activation="relu"))
+        #model.add(layers.Conv2D(filters=106,kernel_size=5,activation="relu"))
+        model.add(layers.MaxPooling2D())
+        model.add(layers.Flatten())
+        model.add(layers.Dense(212, activation="relu"))
+        model.add(layers.Dense(100, activation="relu"))
+        if self.tasknum==0:#classify based on age
+            model.add(layers.Dense(9,activation="softmax"))
+        elif self.tasknum==1:#classify based on gender
+            model.add(layers.Dense(2,activation="softmax"))
+        else:#classify based on race
+            model.add(layers.Dense(7,activation="softmax"))
+        model.summary()
+    
+        model.compile(
+        optimizer=keras.optimizers.SGD(learning_rate=self.lr,momentum=self.mom,nesterov=True),#default lr=0.01, default mom=0.0
+        loss=keras.losses.CategoricalCrossentropy(),
+        metrics=[keras.metrics.CategoricalAccuracy()],
+        )
+        
+        #training and validating
+        print('training and validating')
+        reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=self.lrfactor, patience=1, min_lr=0.0001,)
+        history=model.fit(self.xtrain,self.ytrain,validation_split=0.2,epochs=self.epochs,callbacks=[reduce_lr])#if you don't specify a batch size, it uses 32 for mini-batch GD
+        #testing
+        print('evalauting')
+        results=model.evaluate(self.xval,self.yval)
+        print('predicting')
+        #predicting
+        predictions = model.predict_classes(self.xval)
+        
+        #plotting/final results summary, from https://machinelearningmastery.com/display-deep-learning-model-training-history-in-keras/
+        plt.plot(history.history['categorical_accuracy'])
+        plt.plot(history.history['val_categorical_accuracy'])
+        if self.tasknum==0:#classify based on age
+            plt.title('model accuracy for age classification')
+        elif self.tasknum==1:#classify based on gender
+            plt.title('model accuracy for gender classification')
+        else:#classify based on race
+            plt.title('model accuracy for race classification')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
+
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        if self.tasknum==0:#classify based on age
+            plt.title('model loss for age classification')
+        elif self.tasknum==1:#classify based on gender
+            plt.title('model loss for gender classification')
+        else:#classify based on race
+            plt.title('model loss for race classification')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
+        trainingloss=history.history['loss']
+        validationloss=history.history['val_loss']
+        print('final validation accuracy is %f' %results[1])
+        
+        confmatrix=sklearn.metrics.confusion_matrix(self.yvaltruth,predictions)
+        self.tasktypes=['age confusion matrix','Gender confusion matrix','race confusion matrix']
+        self.tasktypes=self.tasktypes[self.tasknum]
+        cmdisp=sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix=confmatrix,display_labels=self.cm_plot_labels)
+        cmdisp.plot()
+        plt.show()
+        return [trainingloss,validationloss,history,results,confmatrix]
+#####################################################################################################################
+'''
+    def cnntwo(self,tasknum=0,lr=0.5,lrfactor=0.2,mom=0.1,epochs=10):
+        self.tasknum=tasknum
+        self.lr=lr
+        self.lrfactor=lrfactor
+        self.mom=mom
+        self.epochs=epochs
+        self.xtrain=np.expand_dims(self.xtrain.reshape(86744,32,32), axis=3)#reshaping for convolution
+        self.xval=np.expand_dims(self.xval.reshape(10954,32,32), axis=3)#reshaping for convolution
+        
+        
+        
+        
+        
+        #two task specific reworks
+        
+        inputsinputs=keras.Input(shape=(32,32,1))
+        x=layers.Conv2D(filters=40,kernel_size=5,activation="relu")(inputs)
+        x=layers.MaxPooling2D()(x)
+        x=layers.Flatten()(x)
+        y=layers.Dense(100, activation="relu")(x)
+        z=layers.Dense(100, activation="relu")(x)
+        gender=layers.Dense(2,activation="softmax")(y)#classify gender
+        raceoutput=layers.Dense(7,activation="softmax")(z)
+        model=keras.Model(inputs=[inputs]), outputs=[gender,race])
+        model.summary()
+    
+        model.compile(
+        optimizer=keras.optimizers.SGD(learning_rate=self.lr,momentum=self.mom),#default lr=0.01, default mom=0.0
+        loss=keras.losses.CategoricalCrossentropy(),
+        metrics=[keras.metrics.CategoricalAccuracy()],
+        )
+        
+        #training and validating
+        print('training and validating')
+        reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=self.lrfactor, patience=1, min_lr=0.0001,)
+        history=model.fit(self.xtrain,[self.ytgender,self.ytrace],validation_split=0.2,epochs=self.epochs,callbacks=[reduce_lr])#if you don't specify a batch size, it uses 32 for mini-batch GD
+        #testing
+        print('evalauting')
+        results=model.evaluate(self.xval,[self.yvgender,self.yvrace])
+        print('predicting')
+        #predicting
+        predictions = model.predict_classes(self.xval)
+        
+        #plotting/final results summary, from https://machinelearningmastery.com/display-deep-learning-model-training-history-in-keras/
+        plt.plot(history.history['categorical_accuracy'])
+        plt.plot(history.history['val_categorical_accuracy'])
+        if self.tasknum==0:#classify based on age
+            plt.title('model accuracy for age classification')
+        elif self.tasknum==1:#classify based on gender
+            plt.title('model accuracy for gender classification')
+        else:#classify based on race
+            plt.title('model accuracy for race classification')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
+
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        if self.tasknum==0:#classify based on age
+            plt.title('model loss for age classification')
+        elif self.tasknum==1:#classify based on gender
+            plt.title('model loss for gender classification')
+        else:#classify based on race
+            plt.title('model loss for race classification')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
+        trainingloss=history.history['loss']
+        validationloss=history.history['val_loss']
+        print('final validation accuracy is %f' %results[1])
+        
+        confmatrix=sklearn.metrics.confusion_matrix(self.yvaltruth,predictions)
+        self.tasktypes=['age confusion matrix','Gender confusion matrix','race confusion matrix']
+        self.tasktypes=self.tasktypes[self.tasknum]
+        cmdisp=sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix=confmatrix,display_labels=self.cm_plot_labels)
+        cmdisp.plot()
+        plt.show()
+        return [trainingloss,validationloss,history,results,confmatrix]
+
     def vae(self):
         print('init')
-
+'''
 
 #Pre-processing
 PREPROCESS=preprocess()
 #PREPROCESS.firsttimerun()
-train_images,train_labels,val_images,val_labels,categories=PREPROCESS.runeverytime()
+train_images,train_labels,val_images,val_labels=PREPROCESS.runeverytime()
 
 # Task 1, FCNN
-NETWORKS=networks(tasknum=0,xtrain=train_images,ytrain=train_labels,xval=val_images,yval=val_labels,categories=categories)
+NETWORKS=networks(xtrain=train_images,ytrain=train_labels,xval=val_images,yval=val_labels)
 FCNN=NETWORKS.fcnn()
 ## Task 2, CNN
-CNN=NETWORKS.cnn()
+#CNN=NETWORKS.cnn()
 ## Task 3, homebrew CNN
-CNNHOMEBREW=NETWORKS.cnnhomebrew()
+#CNNHOMEBREW=NETWORKS.cnnhomebrew()
 ## Task 4, multitask CNN
-CNNTWO=NETWORKS.cnntwo()
+#CNNTWO=NETWORKS.cnntwo()
 ## Task 5, Variational Auto-Encoder
-VAE=NETWORKS.vae()
+#VAE=NETWORKS.vae()
